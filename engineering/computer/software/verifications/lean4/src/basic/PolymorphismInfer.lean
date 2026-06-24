c47bc619cec4a91a81feeca712ab0e6b6729ff04
AshGrey🥕 <ashgrey.huaier@gmail.com>
2026-06-24 22:16:06 +0800

    :nut_and_bolt: feat(lean4): polymorphism, types argument

---
+++ engineering/computer/software/verifications/lean4/src/basic/PolymorphismInfer.lean
@@ -0,0 +1,17 @@
+structure PPoint (α : Type) where
+  x : α
+  y : α
+
+def replacePPointX {α : Type} (point : PPoint α) (new_x : α) : PPoint α :=
+  { point with x := new_x }
+
+#eval replacePPointX { x := 3, y := 1 } 2
+-- It can be called without providing `Nat` explicitly
+
+def length {α : Type} (list : List α) : Nat :=
+  match list with
+  | [] => 0
+  | _ :: rest => Nat.succ (length rest)
+
+#eval length [1, 2, 3]
+-- It can be called without providing `Nat` explicitly
