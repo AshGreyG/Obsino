cb93f491887f33c3e104fa0109f3826c1a3196d9
AshGrey🥕 <ashgrey.huaier@gmail.com>
2026-07-06 00:45:03 +0800

    :art: chore(code-style): rename theorems in `TheoremProof.lean` as snake case

--- engineering/computer/software/verifications/lean4/src/proof/TheoremProof.lean
+++ engineering/computer/software/verifications/lean4/src/proof/TheoremProof.lean
@@ -9,14 +9,14 @@ section proof
   #check t1 r s             -- r → s → r
   #check t1 (r ∧ s) (r ∨ s) -- r ∧ s → r ∨ s → r ∧ s
 
-  theorem t1Printed : ∀ (p q : Prop), p → q → p :=
+  theorem t1_printed : ∀ (p q : Prop), p → q → p :=
     fun p q (hp : p) (hq : q) => hp
     -- ∀ {p q : Prop}, p → (q → p) is a proposition, its proof is an instance
 
-  theorem t1Simplified : p → q → p :=
+  theorem t1_simplified : p → q → p :=
     fun hp : p => fun hq : q => hp
 
-  theorem t1ShowStatement : p → q → p :=
+  theorem t1_show_statement : p → q → p :=
     fun hp : p =>
     fun hq : q =>
     show p from hp
@@ -26,7 +26,7 @@ section implicit
   variable {p q r s : Prop}
   theorem t2 (hp : p) (hq : q) : p := hp
   axiom hp2 : p
-  theorem t2Implicit : q → p := t2 hp2
+  theorem t2_implicit : q → p := t2 hp2
 
   theorem t3 (h₁ : q → r) (h₂ : p → q) : p → r :=
     fun h₃ : p =>
